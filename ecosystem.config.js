6218a66ec66e136a02160ca9eff9cc154c7e0be2
PROkrastinator <vladykin.190103@gmail.com>
2022-01-21 19:07:01 +0300

    +1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
     apps: [
       {
         name: 'miros-backend-strapi',
-        script: 'NODE_ENV=production npm',
-        args: 'run start',
+        script: 'NODE_ENV=production npm run start',
+        args: '',
       },
     ],
   };
